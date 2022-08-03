7de8b372223a9f0c51baf797afcc9e1424eb2cdd
anahitxachatryan <88240587+anahitxachatryan@users.noreply.github.com>
2022-08-03 20:45:52 +0400

    added new features

--- app.r
+++ app.r
@@ -1,7 +1,19 @@
 library(shiny)
 library(shinythemes)
+library(dplyr)
+library(ggplot2)
 if (!require("DT")) install.packages('DT')
-
+factorize = function(data){
+  data_cat = select(data, is.character)
+  col_names = names(data_cat)
+  data[col_names] = lapply(data[col_names] , as.factor)
+  return (data)
+}
+to_date = function(colname, data){
+  data[,colname]<-as.Date(data[,colname], format="%d-%b-%y") 
+  return (data)
+  
+}
 get_mode = function(col){
   return(names(sort(table(col), decreasing = T, na.last = T)[1]))
 }
@@ -11,13 +23,13 @@ fill_missing_vals = function(data, stat_measure){
   for (i in 1:ncol(data)){
     if(is.numeric(data[,i])){
       if (stat_measure == 'mean'){
-        data[,i][is.na(data[,i])] = mean(data[,i], na.rm = TRUE)
+        data[,i][is.na(data[,i])] = round(mean(data[,i], na.rm = TRUE),digits=2)
       }
       if (stat_measure == 'std'){
-        data[,i][is.na(data[,i])] = sd(data[,i],  na.rm = TRUE)
+        data[,i][is.na(data[,i])] = round(sd(data[,i],  na.rm = TRUE),digits=2)
       }
       if (stat_measure == 'median'){
-        data[,i][is.na(data[,i])] = median(data[,i],  na.rm = TRUE)
+        data[,i][is.na(data[,i])] =round( median(data[,i],  na.rm = TRUE),digits=2)
       }
     }
     else{
@@ -54,7 +66,7 @@ transform_data = function(data, func){
   if (func == 'sqr'){
     for (i in 1:ncol(data)){
       if(is.numeric(data[,i])){
-        data[,i] = data[,i]^2
+        data[,i] = round(data[,i]^2, digits=2)
         
       }
     }
@@ -62,7 +74,7 @@ transform_data = function(data, func){
   if (func == 'log'){
     for (i in 1:ncol(data)){
       if(is.numeric(data[,i])){
-        data[,i] = log(data[,i])
+        data[,i] = round(log(data[,i]), digits=2)
         
       }
     }
@@ -71,7 +83,7 @@ transform_data = function(data, func){
     for (i in 1:ncol(data)){
       if(is.numeric(data[,i])){
         
-        data[,i] = min_max_norm(data[,i])
+        data[,i] = round(min_max_norm(data[,i]), digits=2)
         
       }
     }
@@ -81,7 +93,11 @@ transform_data = function(data, func){
 ###################################################################
 
 
-  ui <- fluidPage(theme = shinytheme("sandstone"),
+  ui <- fluidPage(theme = shinytheme("flatly"),
+                  tags$head(tags$style(HTML('.navbar-static-top {background-color: #ADD8E6;}',
+                                            ".navbar-default .navbar-nav>li>a:hover, .navbar-default .navbar-nav>li>a:focus {color: #6495ED;}",
+                                            '.navbar-default .navbar-nav>.active>a,.navbar-default .navbar-nav>.active>a:hover {color: #6495ED;}',
+                                            '.navbar-default .navbar-nav>.active>a, .navbar-default .navbar-nav>.active>a:hover, .navbar-default .navbar-nav>.active>a:focus {background-color: #ADD8E6;}'))),
       navbarPage(
       "",
       tabPanel("Data",
@@ -105,11 +121,13 @@ transform_data = function(data, func){
                                     "display",
                                     "Display",
                                     inline = T,
-                                    choices = c(Str = 'str',
+                                    choices = c(Structure = 'str',
                                                 Summary = 'summary'),
                                     selected = "str"
                                   ),
-                                  
+                                  # selectInput('dateCol','Select Date column: ', ""),
+                                  selectInput("date", "Select Date column:", ""),
+
                                   # Input: Select number of rows to display ----
                                   radioButtons(
                                     "prev",
@@ -127,7 +145,7 @@ transform_data = function(data, func){
                  #          tags$p("Ready to take the Shiny tutorial? If so"),
                  #          tags$a(href="shiny.rstudio.com/tutorial", "Click Here!")
                  # ),
-                 
+                 htmlOutput('test'),
                  htmlOutput("DataPreview"),
                  DT::dataTableOutput('contents'),
                  htmlOutput("SummaryOfData"),
@@ -139,7 +157,7 @@ transform_data = function(data, func){
                
               
       ),#, # Navbar 1, tabPanel,
-      tabPanel("Preprocessing", 
+      tabPanel("Transform", 
                sidebarPanel(
                  width = 4,
                  
@@ -150,38 +168,69 @@ transform_data = function(data, func){
                  radioButtons(
                    "transform",
                    "Transformation",
-                   inline = T,
+                   
                    choices = c(
-                               Normalize = 'norm',
-                               Logarithmic = "log",
-                               Square = "sqr"),
-                   selected = "norm"
-                 ),
+                     "No Transformations" = "nothing",
+                     Normalize = 'norm',
+                     Logarithm = "log",
+                     Square = "sqr"),
+                   selected = "nothing"
+                 ),                 # conditionalPanel(
+                 #   condition = "transform",
+                 #   checkboxGroupInput("transform", "Transformation",
+                 #                      choices = c("norm", "log", "sqr"), selected = FALSE
+                 #   ),
+                 # ),
+                 
                  # checkboxInput("rm_outl", "Remove Outliers",TRUE),
                  conditionalPanel(
                    condition = "rm_outl",
                    checkboxGroupInput("outliers", "Outliers",
-                                      choices = c("Remove Outliers"), selected = "Remove Outliers"
+                                      choices = c("Remove Outliers"), selected = FALSE
                                       ),
-               )
+               ),
+               selectInput("OutlierCol", "Choose column:", ""),
+               selectInput("Col", "Choose cokumn:", choices  = c("OPEN", "CLOSE")),
                  
                  
                
                 
                  ),
                mainPanel(
-                 
+  
                  htmlOutput("header_preprocess"),
                  DT::dataTableOutput('data_transformed'),
-                 DT::dataTableOutput('outlier_removed_table'),
-                 htmlOutput('test'),
+                 # htmlOutput('test'),
+                 plotOutput("boxplotOutliers")
                  #,
                  # textOutput('text'),
                  # plotOutput("plot")
                )# mainPanel,
                ),
-      tabPanel("Data Transformation"),
-      tabPanel("Plots")
+      tabPanel("Explore",
+               sidebarPanel(
+                 selectInput("groupby", "Column to group by:", "", multiple = TRUE),
+                 selectInput("aggregate", "Column to aggregate:", "",multiple = TRUE),
+                 selectInput("func", "Aggregate by:", choices = c(mean = "mean",
+                                                                  std = "std",
+                                                                  median = "median",
+                                                                  count = "count",
+                                                                  max = "max",
+                                                                  min = "min"),multiple = TRUE)
+               ),
+               mainPanel(
+                 htmlOutput("header_agg"),
+                 DT::dataTableOutput('transformed_table'),
+               )
+               ),
+
+      tabPanel("Histogram"),
+      tabPanel("Scatter Plot"),
+      tabPanel("Line Plot"),
+      tabPanel("Box Plot"),
+      tabPanel("Bar Plot"),
+      tabPanel("Heatmap"),
+      
       
     ) # navbarPage
   ) # fluidPage
@@ -189,16 +238,88 @@ transform_data = function(data, func){
   
   # Define server function  
   server <- function(input, output, session) {
-    myData <- reactive({
-      inFile <- input$file1
-      if (is.null(inFile)) {
-        return(mpg)
+
+
+    inFile <- reactive({
+      if (is.null(input$file1)) {
+        return(NULL)
+      } else {
+        input$file1
       }
-      data <- read.csv(inFile$datapath, header = TRUE)
-      data
     })
     
+    myData <- reactive({
+      
+      if (is.null(inFile())) {
+        return(NULL)
+      } else {
+        data = read.csv(inFile()$datapath)
+        data = factorize(data)
+      }
+      if (input$date != ""){
+        data = fill_missing_vals(data, "median")
+        col = input$date
+        data = to_date(col, data)
+      }
+      # if(input$factorizeCol == "Yes"){
+      # 
+      #   data = factorize(data)
+      # }
+      # if(is.null(input$factorizeCol)){
+      #   
+      #   data  = read.csv(inFile()$datapath)
+      # }
+      # 
+
+      return (data)
+    })
+    
+    
+    take_data <- reactive({
+      
+      if (is.null(inFile())) {
+        return(NULL)
+      } else {
+        read.csv(inFile()$datapath)
+      }
+
+    })
+    myData_numerics <- reactive({
+      if (is.null(inFile())) {
+        return(NULL)
+      } else {
+        df = read.csv(inFile()$datapath)
+        data_num = select_if(df, is.numeric)
+        return(data_num)
+      }
+    })
+    
+    observe({
+      updateSelectInput(
+        session,
+        "date",
+        choices=c("",names(take_data())))
+      
+      updateSelectInput(
+        session,
+        "groupby",
+        choices=names(take_data()))
+      updateSelectInput(
+        session,
+        "aggregate",
+        choices=names(myData_numerics()))
+      updateSelectInput(
+        session,
+        "OutlierCol",
+        choices=names(take_data()))
+      
+      
+    })
+    
+
+
     output$contents <- DT::renderDataTable({
+
       if (input$prev == "head") {
         return(head(myData()))
       }
@@ -207,33 +328,37 @@ transform_data = function(data, func){
     
     output$summary <- renderPrint({
       if (input$display == 'str') {
-        str(myData())
+        glimpse(myData())
         
       }
       else{
         summary(myData())
       }
       
+      
     })
     
-    output$select <- renderUI({
-      df <- myData()
-      selectInput("variable", "Variable:", names(df))
-      
-      
-    })
+    # output$select <- renderUI({
+    #   df <- myData()
+    #   selectInput("variable", "Variable:", names(df))
+    #   
+    #   
+    # })
     output$DataPreview <- renderUI({
+      
       HTML("<h2>Data Preview</h2>")
     })
     output$SummaryOfData <- renderUI({
-      if (input$display == 'summary') {
+
         HTML("<h2>Summary</h2>")
-      }
-      else{
-        HTML("<h2>STR</h2>")
-      }
+
     })
-    
+    # output$test <- renderUI({
+    # 
+    #     HTML(paste(input$DateCol),"aa")
+    # 
+    #   
+    # })
     
     
     
@@ -242,7 +367,21 @@ transform_data = function(data, func){
     #   df <- df[, input$variable]
     #   hist(df)
     # })
-    
+    output$header_agg <- renderUI({
+      
+      HTML("<h2>Data aggregation</h2>")
+      
+    })
+    output$boxplotOutliers <- renderPlot({ ################
+
+      data=myData()
+      data = fill_missing_vals(data, "median")
+      col = input$OutlierCol
+      # x_aes = data$col
+      ggplot(data=data, aes(x=data$col, y=data$HIGH))+ geom_boxplot()
+      # ggplot()+geom_histogram(aes(x=col), bins = 50)
+
+    })
     output$header_preprocess <- renderUI({
 
       HTML("<h2>Data Preporcessing</h2>")
@@ -272,12 +411,12 @@ transform_data = function(data, func){
       if(is.null(input$outliers) == FALSE){
         data = outlier_detection(data)
       }
-
-       if (input$transform == 'norm'){
-
+      
+      if (input$transform == 'norm'){
+        
         data = transform_data(data, 'norm')
         
-       }
+      }
       if (input$transform == 'log'){
         
         data = transform_data(data, 'log')
@@ -288,23 +427,35 @@ transform_data = function(data, func){
         data = transform_data(data, 'sqr')
         
       }
-
-
+      if (input$transform == 'nothing'){
+        
+        data = fill_missing_vals(data, "mean")
+        
+      }
       
-        
-
-        
+      
+      
+      
+      
       # if (is.null(input$outliers)){
       #   data = data_old
       # }
-
+      
       return(data)
       
     }, options = list(scrollX = T))
 
-    # output$test <- renderUI({
-    #    
-    # })
+    output$test <- renderUI({
+      if (input$factorizeCol =="Yes"){
+        
+        # col = input$date
+        # data = to_data(col)
+        HTML(paste("fone"))
+      }
+ 
+  # HTML())
+  # HTML(paste(class(input$Col), "bb"))
+    })
     
     
   }
---
+++ test.R
@@ -0,0 +1,60 @@
+library(ggplot2)
+library(dplyr)
+data=read.csv("oil.csv")
+# ggplot(data=data, aes(y=CLOSE, x=""))+ geom_boxplot()
+
+
+to_date = function(colname, data){
+  data[,colname]<-as.Date(data[,colname], format="%d-%b-%y") 
+  return (data)
+  
+}
+data=to_date("DATE", data)
+head(data)
+
+as.Date("7-Oct-12", format = "%d-%b-%y")
+data=read.csv("Car_company_customers.csv")
+
+factorize = function(data){
+  data_cat = select(data, is.character)
+  col_names = names(data_cat)
+  data[col_names] = lapply(data[col_names] , as.factor)
+  return (data)
+}
+
+
+data = factorize(data)
+str(data)
+
+
+data=read.csv("oil.csv")
+
+get_mode = function(col){
+  return(names(sort(table(col), decreasing = T, na.last = T)[1]))
+}
+
+
+fill_missing_vals = function(data, stat_measure){
+  
+  for (i in 1:ncol(data)){
+    if(is.numeric(data[,i])){
+      if (stat_measure == 'mean'){
+        data[,i][is.na(data[,i])] = round(mean(data[,i], na.rm = TRUE))
+      }
+      if (stat_measure == 'std'){
+        data[,i][is.na(data[,i])] = round(sd(data[,i],  na.rm = TRUE))
+      }
+      if (stat_measure == 'median'){
+        data[,i][is.na(data[,i])] =round( median(data[,i],  na.rm = TRUE))
+      }
+    }
+    else{
+      data[,i][is.na(data[,i])] = get_mode(data[,i])
+    }
+  }
+  
+  return(data)
+}
+
+data = fill_missing_vals(data, "std")
+head(data)
